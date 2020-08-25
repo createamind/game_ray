ac80b8f7c242bc76dce7f4d09cb0654208c3049b
createamind <hr@createamind.com>
2020-08-25 09:41:06 +0800

    tun para

--- c.sh
+++ c.sh
@@ -1,9 +1,10 @@
 
-for((i=1;i<=40;i++));  
+for((i=1;i<=80;i++));  
 do
 
 	echo 'echo `date`'
-	echo 'CUDA_VISIBLE_DEVICES="0" python ptest.py  --test_days 120 --num_workers 120 --checkpoint="/home/jz/tmp/PPO_TradingEnv_0_2020-08-12_16-23-48i3lk_4k2/checkpoint_'$i'0/checkpoint-'$i'0"   > logs8.'$i'0'
-	#echo  'CUDA_VISIBLE_DEVICES="0" python ptest.py  --test_days 120 --num_workers 120 --checkpoint="/home/jz/tmp/PPO_TradingEnv_0_2020-08-12_16-23-06q9ortuzz/checkpoint_'$i'0/checkpoint-'$i'0"    > logs8.'$i'0'
+	echo 'pkill -f ray;sleep 0.1'
+	#echo 'CUDA_VISIBLE_DEVICES="0" python ptest.py  --test_days 120 --num_workers 120 --checkpoint="/home/jz/tmp/PPO_TradingEnv_0_2020-08-12_16-23-48i3lk_4k2/checkpoint_'$i'0/checkpoint-'$i'0"   > logs8.'$i'0'
+	echo  'CUDA_VISIBLE_DEVICES="0" python ptest.py  --test_days 120 --num_workers 120 --checkpoint="/home/jz/tmp/PPO_TradingEnv_0_2020-08-12_16-23-06q9ortuzz/checkpoint_'$i'0/checkpoint-'$i'0"    > logs4.'$i'0'
 	echo 'echo `date`'
 done
